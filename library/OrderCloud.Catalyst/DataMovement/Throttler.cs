765cdd619a73d5dba6bf3bb89d141ca53af64a04
Oliver Heywood <oheywood@four51.com>
2021-02-15 13:56:18 -0600

    update throttler

--- library/OrderCloud.Catalyst/DataMovement/Throttler.cs
+++ library/OrderCloud.Catalyst/DataMovement/Throttler.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace OrderCloud.Catalyst
@@ -19,17 +20,56 @@ namespace OrderCloud.Catalyst
 		/// <param name="doWorkAsync"></param>
 		/// <param name="maxConcurrent">Maximum number of tasks that can be running concurrently at any given time. Default is 50.</param>
 		/// <returns></returns>
-	    public static async Task ForEachAsync<T>(IEnumerable<T> data, Func<T, Task> doWorkAsync, int maxConcurrent = 50) {
-			var tasks = new List<Task>();
-			foreach (var x in data) {
-				if (tasks.Count >= maxConcurrent) { // do a quick check first
-					// count running tasks. if at or over limit, wait for one (or more) to complete before letting the next one in
-					while (tasks.Count(t => !t.IsCompleted && !t.IsFaulted) > maxConcurrent)
-						await Task.WhenAny(tasks).ConfigureAwait(false);
-				}
-				tasks.Add(doWorkAsync(x));
-			}
-			await Task.WhenAll(tasks).ConfigureAwait(false);
-		}
+        public static async Task RunAsync<T>(IEnumerable<T> items, int minPause, int maxConcurrent, Func<T, Task> op)
+        {
+            using (var sem = new SemaphoreSlim(maxConcurrent))
+            {
+                async Task RunOneAsync(T item)
+                {
+                    try { await op(item); }
+                    finally { sem.Release(); }
+                }
+
+                var tasks = new List<Task>();
+                foreach (var item in items)
+                {
+                    if (tasks.Any()) // avoid pausing before the first one
+                        await Task.WhenAll(sem.WaitAsync(), Task.Delay(minPause)); // wait until we're under the concurrency limit AND at least minPause has passed
+                    tasks.Add(RunOneAsync(item));
+                }
+                await Task.WhenAll(tasks);
+            }
+        }
+
+        /// <summary>
+        /// Perform concurrent asynchronous work on a set of data, but limit concurrency to some maximum number of running tasks.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="data"></param>
+        /// <param name="doWorkAsync"></param>
+        /// <param name="maxConcurrent">Maximum number of tasks that can be running concurrently at any given time. Default is 50.</param>
+        /// <returns></returns>
+        public static async Task<IList<TOutput>> RunAsync<TInput, TOutput>(IEnumerable<TInput> items, int minPause, int maxConcurrent, Func<TInput, Task<TOutput>> op)
+        {
+            using (var sem = new SemaphoreSlim(maxConcurrent))
+            {
+                async Task<TOutput> RunOneAsync(TInput item)
+                {
+                    try { return await op(item); }
+                    finally { sem.Release(); }
+                }
+
+                var tasks = new List<Task<TOutput>>();
+                foreach (var item in items)
+                {
+                    if (tasks.Any()) // avoid pausing before the first one
+                        await Task.WhenAll(sem.WaitAsync(), Task.Delay(minPause)); // wait until we're under the concurrency limit AND at least minPause has passed
+                    tasks.Add(RunOneAsync(item));
+                }
+
+                var result = await Task.WhenAll(tasks);
+                return result.ToList();
+            }
+        }
     }
 }

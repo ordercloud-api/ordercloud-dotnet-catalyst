432abdf10e3afce1ba259ceff501a64b044d93b8
Oliver Heywood <oliver.heywood@sitecore.com>
2022-01-26 13:54:54 -0600

    remove duplicates

--- library/OrderCloud.Catalyst/Integrations/OCIntegrationCommand.cs
+++ library/OrderCloud.Catalyst/Integrations/OCIntegrationCommand.cs
@@ -5,16 +5,10 @@ using System.Text;
 namespace OrderCloud.Catalyst
 {
 	/// <summary>
-	/// A base class that all Integration Config classes should extend. Contains environment variables needed for that integration.
-	/// </summary>
-	public class OCIntegrationConfig 
-	{
-	}
-
-	/// <summary>
 	/// A base class that all Integration Command classes should extend. Exposes methods that are the behaviors of the integration.
 	/// </summary>
 	public class OCIntegrationCommand
 	{
+
 	}
 }
--- library/OrderCloud.Catalyst/Integrations/OCIntegrationConfig.cs
+++ library/OrderCloud.Catalyst/Integrations/OCIntegrationConfig.cs
@@ -11,10 +11,5 @@ namespace OrderCloud.Catalyst
 	{
 	}
 
-	/// <summary>
-	/// A base class that all Integration Command classes should extend. Exposes methods that are the behaviors of the integration.
-	/// </summary>
-	public class OCIntegrationCommand
-	{
-	}
+
 }

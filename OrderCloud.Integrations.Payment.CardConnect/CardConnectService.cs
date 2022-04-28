c53a4c4db9f391f724cd6f754af00a0d55962517
DJ Steinmetz <dsteinmetz@four51.com>
2022-04-28 13:55:32 -0500

    CardConnect update namespace error

--- OrderCloud.Integrations.Payment.CardConnect/CardConnectService.cs
+++ OrderCloud.Integrations.Payment.CardConnect/CardConnectService.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace OrderCloud.Integrations.Payment.BlueSnap
+namespace OrderCloud.Integrations.Payment.CardConnect
 {
 	public class CardConnectService : OCIntegrationService, ICreditCardProcessor
 	{

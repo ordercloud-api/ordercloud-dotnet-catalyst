c365ef2401417b0b29365a7809c480950a1f6fef
Alexa Snyder <alexa.snyder@sitecore.com>
2022-05-06 11:19:28 -0500

    remove unused config property

--- OrderCloud.Integrations.Payment.Stripe/StripeConfig.cs
+++ OrderCloud.Integrations.Payment.Stripe/StripeConfig.cs
@@ -10,9 +10,5 @@ namespace OrderCloud.Integrations.Payment.Stripe
         public override string ServiceName { get; } = "Stripe";
         [RequiredIntegrationField]
         public string SecretKey { get; set; }
-        // not sure if this is needed
-        // On the client-side. Can be publicly-accessible in your web or mobile app’s client-side code (such as checkout.js) to tokenize payment information such as with Stripe Elements. By default, Stripe Checkout tokenizes payment information.
-        //[RequiredIntegrationField]
-        //public string PublishableKey { get; set; }
     }
 }

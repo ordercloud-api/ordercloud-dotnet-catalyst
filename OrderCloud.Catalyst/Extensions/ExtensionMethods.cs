ab4945a4def19ab5ba3a3fe7911c24da44f02edb
Erin <erincdustin@gmail.com>
2025-02-27 13:18:17 -0800

    Address security vulnerabilities

--- OrderCloud.Catalyst/Extensions/ExtensionMethods.cs
+++ OrderCloud.Catalyst/Extensions/ExtensionMethods.cs
@@ -2,7 +2,6 @@
 using Flurl.Util;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.Features;
-using Microsoft.AspNetCore.Mvc.Infrastructure;
 using Microsoft.Extensions.DependencyInjection;
 using OrderCloud.SDK;
 using System;
